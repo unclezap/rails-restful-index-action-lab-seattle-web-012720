745881e83e306ff0a948742b6212506b2f171b13
unclezap <zeb.blackwell@gmail.com>
2020-02-21 14:52:11 -0800

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,17 @@
+class StudentsController < ApplicationController
+    def index
+        puts "afsdfsfsdfaskfghaiodfhjgoidjfajksndfoagjdfbgioshdfbgsijdnfgkjsndfg"
+        puts "afsdfsfsdfaskfghaiodfhjgoidjfajksndfoagjdfbgioshdfbgsijdnfgkjsndfg"
+        puts "afsdfsfsdfaskfghaiodfhjgoidjfajksndfoagjdfbgioshdfbgsijdnfgkjsndfg"
+        puts "afsdfsfsdfaskfghaiodfhjgoidjfajksndfoagjdfbgioshdfbgsijdnfgkjsndfg"
+        puts "afsdfsfsdfaskfghaiodfhjgoidjfajksndfoagjdfbgioshdfbgsijdnfgkjsndfg"
+        puts "afsdfsfsdfaskfghaiodfhjgoidjfajksndfoagjdfbgioshdfbgsijdnfgkjsndfg"
+        puts "afsdfsfsdfaskfghaiodfhjgoidjfajksndfoagjdfbgioshdfbgsijdnfgkjsndfg"
+        puts "afsdfsfsdfaskfghaiodfhjgoidjfajksndfoagjdfbgioshdfbgsijdnfgkjsndfg"
+        puts "afsdfsfsdfaskfghaiodfhjgoidjfajksndfoagjdfbgioshdfbgsijdnfgkjsndfg"
+        # byebug
+        @student = Student.all
+        render "index"
+    end
+
+end
